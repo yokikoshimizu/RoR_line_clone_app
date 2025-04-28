8729c821c4544419fe2f81d92985c1d5728f7fed
yokikoshimizu <yokikoshimizu@gmail.com>
2025-04-29 08:17:09 +0900

    MessageControllerにチャット機能基本処理を実装(途中)

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,7 +1,14 @@
 class MessageController < ApplicationController
   before_action :authenticate_user!
   before_action :set_receiver
+
+  # この部分のコードが調べてもいまいち全体的に理解できませんでした
   def index
+    @messages = Message.where(
+      "(sender_id = :current AND receiver_id = :receiver) OR (sender_id = :receiver AND receiver_id = :current)",
+      current: current_user.id,
+      receiver: @receiver.id
+    ).order(:created_at)
   end
 
   def create
